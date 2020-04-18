ef53a498949db4c10078c2118ea25c2c1954c8eb
Brett Sun <qisheng.brett.sun@gmail.com>
2020-04-18 18:20:05 +0200

    Analytics: migrate to analytics.aragon.org

--- docs/javascript/extra_js.js
+++ docs/javascript/extra_js.js
@@ -1,11 +1,17 @@
-var _paq = window._paq || [];
-/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
+var Countly = Countly || {};
+Countly.q = Countly.q || [];
+//provide countly initialization parameters
+Countly.app_key = 'd32b935378b6cecc6fefe00395f856e7a54c802f';
+Countly.url = 'https://analytics.aragon.org/';
+Countly.inactivity_time = 10;
+Countly.q.push(['track_sessions']);
+Countly.q.push(['track_pageview']);
+Countly.q.push(['track_clicks']);
+Countly.q.push(['track_errors']);
 (function() {
-  var u="//arastats.eu/";
-  _paq.push(['setTrackerUrl', u+'matomo.php']);
-  _paq.push(['setSiteId', '2']);
-  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  var cly = document.createElement('script'); cly.type = 'text/javascript';
+  cly.async = true;
+  cly.src = 'https://analytics.aragon.org/sdk/web/countly.min.js';
+  cly.onload = function(){Countly.init()};
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(cly, s);
 })();
